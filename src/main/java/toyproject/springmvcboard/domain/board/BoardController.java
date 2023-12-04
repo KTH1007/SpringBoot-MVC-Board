cef17e9fae95e2a740167da31bf328a006ea98a7
KTH1007 <rlaxogjs2684@gmail.com>
2023-12-04 16:14:25 +0900

    Feat : 게시글 Validation

--- src/main/java/toyproject/springmvcboard/domain/board/BoardController.java
+++ src/main/java/toyproject/springmvcboard/domain/board/BoardController.java
@@ -1,8 +1,12 @@
 package toyproject.springmvcboard.domain.board;
 
+import jakarta.validation.Valid;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,6 +16,8 @@ import java.util.List;
 
 @Controller
 @RequestMapping("/board")
+@Log4j2
+@Validated
 public class BoardController {
 
     private final BoardService boardService;
@@ -34,7 +40,12 @@ public class BoardController {
     }
 
     @PostMapping("/form")
-    public String saveBoard(@ModelAttribute BoardDTO boardDTO) {
+    public String saveBoard(@Valid @ModelAttribute BoardDTO boardDTO, BindingResult bindingResult) {
+        // 검증 실패시 다시 입력폼으로
+        if (bindingResult.hasErrors()) {
+            log.info("bindingResult = {}", bindingResult.toString());
+            return "board/form";
+        }
         boardService.save(boardDTO);
         return "redirect:/board/list";
     }
--- src/main/java/toyproject/springmvcboard/domain/board/BoardDTO.java
+++ src/main/java/toyproject/springmvcboard/domain/board/BoardDTO.java
@@ -1,16 +1,23 @@
 package toyproject.springmvcboard.domain.board;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
 
 @Getter
 @Setter
 @NoArgsConstructor
 public class BoardDTO {
     private long id;
+    @NotNull
+    @NotBlank
     private String title;
+    @NotNull
+    @NotBlank
     private String content;
 
     @Builder
